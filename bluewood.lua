d4dc41971da2676ab3709b340a5186cb95b16469
bacaman23 <144810321+bacaman23@users.noreply.github.com>
2023-12-22 14:52:00 +0000

    Update bluewood.lua

--- bluewood.lua
+++ bluewood.lua
@@ -1,28 +1,26 @@
 print("code loading")
-local Tree
+_G.Tree = nil
 local StarterGui = game:GetService("StarterGui")
-for i, v in pairs(workspace:GetChildren()) do
-	if v.Name == "TreeRegion" then
-		for k, l in pairs(v:GetChildren()) do
-			if l:FindFirstChild("TreeClass") and l:FindFirstChild("WoodSection") and l:FindFirstChild("Owner") then
-				if (l.TreeClass.Value == "BlueSpruce") and l.Owner.Value == nil then
-					Tree = l
+while _G.Tree == nil
+	wait(2)
+	for i, v in pairs(workspace:GetChildren()) do
+		if v.Name == "TreeRegion" then
+			for k, l in pairs(v:GetChildren()) do
+				if l:FindFirstChild("TreeClass") and l:FindFirstChild("WoodSection") and l:FindFirstChild("Owner") then
+					if (l.TreeClass.Value == "BlueSpruce") and l.Owner.Value == nil then
+						_G.Tree = l
+					end
 				end
 			end
 		end
 	end
 end
-if Tree == nil then
-	return
-end
-repeat
-	wait()
+if Tree > 0 then
 	game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = Tree["WoodSection"].CFrame
-	game:GetService("ReplicatedStorage").Interaction.ClientIsDragging:FireServer(Tree)
-until Tree["Owner"].Value == game.Players.LocalPlayer
-StarterGui:SetCore("SendNotification", {
-    Title = "V2322",
-    Text = "Found Tree Type[BlueSpruce]",
-    Icon = "rbxassetid://6238540373",
-    Duration = 2,
-})
+	StarterGui:SetCore("SendNotification", {
+    	Title = "V2322",
+    	Text = "Found Tree Type[BlueSpruce]",
+    	Icon = "rbxassetid://6238540373",
+    	Duration = 2,
+	})
+end
