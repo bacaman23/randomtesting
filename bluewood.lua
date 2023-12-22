5517a2ff34b9104e3d5ee5c6552754bcc63d3ee5
bacaman23 <144810321+bacaman23@users.noreply.github.com>
2023-12-22 14:41:50 +0000

    Add files via upload

---
+++ bluewood.lua
@@ -0,0 +1,27 @@
+local Tree
+local StarterGui = game:GetService("StarterGui")
+for i, v in pairs(workspace:GetChildren()) do
+	if v.Name == "TreeRegion" then
+		for k, l in pairs(v:GetChildren()) do
+			if l:FindFirstChild("TreeClass") and l:FindFirstChild("WoodSection") and l:FindFirstChild("Owner") then
+				if (l.TreeClass.Value == "BlueSpruce") and l.Owner.Value == nil then
+					Tree = l
+				end
+			end
+		end
+	end
+end
+if Tree == nil then
+	return
+end
+repeat
+	wait()
+	game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = Tree["WoodSection"].CFrame
+	game:GetService("ReplicatedStorage").Interaction.ClientIsDragging:FireServer(Tree)
+until Tree["Owner"].Value == game.Players.LocalPlayer
+StarterGui:SetCore("SendNotification", {
+    Title = "V2322",
+    Text = "Found Tree Type[BlueSpruce]",
+    Icon = "rbxassetid://6238540373",
+    Duration = 2,
+})
